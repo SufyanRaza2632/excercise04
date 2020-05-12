df2b3fc3c3c10c8f2f8571af67eca315179cd051
sufyan <msufyanraza.26@gmail.com>
2020-05-12 20:21:39 +0500

    in this commit the digit sum is calculated for an input from user at run time

--- matriculation_number.py
+++ matriculation_number.py
@@ -1,11 +1,11 @@
-matriculation_number = "00810765"                                     #Matriculation Number in String 
+matriculation_number = input ("enter any number")                     #Number in String 
 matriculation_number = int(matriculation_number)                      #convert string to integer
 print (matriculation_number)
 sum_of_number = 0                                                     #create an integer variable to store the sum 
 
 while matriculation_number != 0:
-    sum_of_number = sum_of_number + (matriculation_number % 10)       #adds the last digit of matriculation number to sum
-    matriculation_number = int (matriculation_number / 10)            #drops the last digit of matriculation number
+    sum_of_number = sum_of_number + (matriculation_number % 10)       #adds the last digit of number to sum
+    matriculation_number = int (matriculation_number / 10)            #drops the last digit of  number
 
-print("The digit sum of matriculation number is "
+print("The digit sum of number is "
       + str(sum_of_number))                                           #convert the sum back to string before concatenating
