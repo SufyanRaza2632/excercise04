38c9791f9aa0365597c23fe8038412b365a76345
sufyan <msufyanraza.26@gmail.com>
2020-05-12 20:49:51 +0500

    In this commit the difference of the input number from its digit sum is calculated

--- matriculation_number.py
+++ matriculation_number.py
@@ -1,11 +1,14 @@
 matriculation_number = input ("enter any number")                     #Number in String 
 matriculation_number = int(matriculation_number)                      #convert string to integer
 print (matriculation_number)
+number = matriculation_number                                         #create another variable to store the value of entered number
 sum_of_number = 0                                                     #create an integer variable to store the sum 
 
 while matriculation_number != 0:
     sum_of_number = sum_of_number + (matriculation_number % 10)       #adds the last digit of number to sum
     matriculation_number = int (matriculation_number / 10)            #drops the last digit of  number
 
-print("The digit sum of number is "
-      + str(sum_of_number))                                           #convert the sum back to string before concatenating
+
+difference = number - sum_of_number                                   #subtracts digit sum from the orignal number
+print("The difference of number is "
+      + str(difference))                                              #convert the sum back to string before concatenating
